7d6ccb384c1307a7f6b3ce0bae4dc64bfb9402a6
Kaniya Wolverine <Abhisheksoman@eoxon.com>
2024-10-18 14:13:59 +0530

    updated damage product

--- production_app/models.py
+++ production_app/models.py
@@ -8,6 +8,7 @@ class water_Finished_goods_category(models.Model):
         return self.category_name
 
 
+
 class water_Finished_Goods(models.Model):
     category = models.ForeignKey(water_Finished_goods_category, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=150, null=True,unique=True)
@@ -17,7 +18,14 @@ class water_Finished_Goods(models.Model):
     total_stock = models.IntegerField(default=0)  # Field to track total stock
 
 
+class Damaged_good_category(models.Model):
+    category_name = models.CharField(max_length=150,null=True)
+
+    def __str__(self):
+        return self.category_name
+
 class damaged_Goods(models.Model):
-    name = models.CharField(max_length=150, null=True,unique=True)
+    category = models.ForeignKey(Damaged_good_category,on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=150, null=True)
     image = models.ImageField(upload_to='images/',null=True)
-    description =  models.CharField(max_length=200,null=True)
+    description = models.CharField(max_length=200,null=True)
--- production_app/views.py
+++ production_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from Glenda_App.models import Menu
-from production_app.forms import water_category_Form, finishedwaterForm, damaged_goods_Form
+from production_app.forms import water_category_Form, finishedwaterForm, damaged_goods_Form,DamagedForm,update_damaged_goods_Form
 from django.contrib import messages
 
-from production_app.models import water_Finished_goods_category, water_Finished_Goods, damaged_Goods
+from production_app.models import water_Finished_goods_category, water_Finished_Goods, damaged_Goods,Damaged_good_category
 
 
 # Create your views here.
@@ -31,6 +31,8 @@ def addwater_category(request):
 
             return redirect('add_category')
     return render(request,'production/create_water_category.html',{'form':form,'menus':menus})
+
+
 
 def create_water(request):
     menus = Menu.objects.prefetch_related('submenus').all()
@@ -76,6 +78,21 @@ def delete_goods(request, id):
 
 
 
+def add_damaged_good_category(request):
+    menus = Menu.objects.prefetch_related('submenus').all()
+    if request.method == "POST":
+        form = DamagedForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Successfull')
+
+            return redirect('add_category')
+        else:
+            return render(request,'production/damaged_good_category.html',{'form':form, menus:'menus'})
+    else:
+        form = DamagedForm(request.POST)
+        return render(request, 'production/damaged_good_category.html', {'form': form, menus: 'menus'})
 def damaged_goods(request):
     menus = Menu.objects.prefetch_related('submenus').all()
     dd=damaged_Goods.objects.all()
@@ -86,7 +103,7 @@ def Add_damagedgoods(request):
     menus = Menu.objects.prefetch_related('submenus').all()
 
     if request.method == 'POST':
-        form = damaged_goods_Form(request.POST, request.FILES)
+        form = damaged_goods_Form(request.POST,request.FILES)
         if form.is_valid():
             form.save()
             return redirect('damaged_goods')  # Replace 'Route' with the actual view name or URL pattern
@@ -94,3 +111,29 @@ def Add_damagedgoods(request):
         form = damaged_goods_Form()
 
     return render(request, 'production/add_damaged_goods.html', {'form': form, 'menus': menus})
+
+def damage_delete(request,id):
+    menus = Menu.objects.prefetch_related('submenus').all()
+    details = get_object_or_404(damaged_Goods,id=id)
+
+    if request.method == 'POST':
+        try:
+            details.delete()
+            return redirect('damaged_goods')
+        except Exception as e:
+            print(e)
+    return render(request,'production/damage_delete.html',{menus:'menus'})
+
+def update_damage(request,id):
+    menus = Menu.objects.prefetch_related('submenus').all()
+    details = get_object_or_404(damaged_Goods, id=id)
+
+    if request.method == 'POST':
+        form = update_damaged_goods_Form(request.POST,instance=details)
+        if form.is_valid():
+            form.save()
+            return redirect('damaged_goods')
+
+    else:
+        form = update_damaged_goods_Form(instance=details)
+    return render(request,'production/update_damage.html',{details:'details',menus:'menus','form':form})
